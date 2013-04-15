ba525af42937120e3333a4a232679cdcdaaf7dd8
Dabo Ross <dabodialga@gmail.com>
2013-04-14 23:20:14 -0700

    Worked on things

--- src/net/daboross/bukkitdev/banreport/commandreactors/NewReportCommand.java
+++ src/net/daboross/bukkitdev/banreport/commandreactors/NewReportCommand.java
@@ -1,9 +1,25 @@
 package net.daboross.bukkitdev.banreport.commandreactors;
 
+import net.daboross.bukkitdev.banreport.bukkit.BanReportBukkitMain;
+import net.daboross.bukkitdev.banreport.database.BanReportXMLDatabase;
+import net.daboross.bukkitdev.commandexecutorbase.CommandExecutorBase;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+
 /**
  *
  * @author daboross
  */
-public class NewReportCommand {
-    
+public class NewReportCommand implements CommandExecutorBase.CommandReactor {
+
+    private final BanReportXMLDatabase database;
+
+    public NewReportCommand(BanReportXMLDatabase database) {
+        this.database = database;
+    }
+
+    @Override
+    public void runCommand(CommandSender sender, Command mainCommand, String mainCommandLabel, String subCommand, String subCommandLabel, String[] subCommandArgs, CommandExecutorBase.CommandExecutorBridge executorBridge) {
+        throw new UnsupportedOperationException("NewReportCommand: Not Created Yet!: runCommand");
+    }
 }
--- src/net/daboross/bukkitdev/banreport/commandreactors/ReadReportsCommand.java
+++ src/net/daboross/bukkitdev/banreport/commandreactors/ReadReportsCommand.java
@@ -1,5 +1,6 @@
 package net.daboross.bukkitdev.banreport.commandreactors;
 
+import net.daboross.bukkitdev.banreport.database.BanReportXMLDatabase;
 import net.daboross.bukkitdev.commandexecutorbase.CommandExecutorBase;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
@@ -9,6 +10,12 @@ import org.bukkit.command.CommandSender;
  * @author daboross
  */
 public class ReadReportsCommand implements CommandExecutorBase.CommandReactor {
+
+    private final BanReportXMLDatabase database;
+
+    public ReadReportsCommand(BanReportXMLDatabase database) {
+        this.database = database;
+    }
 
     @Override
     public void runCommand(CommandSender sender, Command mainCommand, String mainCommandLabel, String subCommand, String subCommandLabel, String[] subCommandArgs, CommandExecutorBase.CommandExecutorBridge executorBridge) {
